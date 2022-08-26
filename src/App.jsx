0cdeb2c063a63a4a4524a6e7f9f06fdfe9f167f4
KevinBlakeLopez <80014258+KevinBlakeLopez@users.noreply.github.com>
2022-08-25 18:50:21 -0700

    added the 'create' of the CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 function App() {
   const [people, setPeople] = useState([]);
   const [search, setSearch] = useState("");
+  const [formData, setFormData] = useState({ first_name: "" });
 
   const fetchAllPeople = () => {
     (async () => {
@@ -16,6 +17,9 @@ function App() {
     })();
   };
 
+  useEffect(fetchAllPeople, []);
+
+  // for searchbox
   const handleSubmit = () => {
     if (!search) fetchAllPeople();
     setPeople(people.filter((person) => person.first_name === search));
@@ -26,11 +30,48 @@ function App() {
     setSearch(target.value);
   };
 
-  useEffect(fetchAllPeople, []);
+  // for adding person to database
+  const handleAddPerson = ({ target }) => {
+    setFormData({ first_name: target.value });
+  };
+
+  const fetchAddPerson = async () => {
+    try {
+      const response = await fetch("http://localhost:4000/person", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const newPerson = await response.json();
+      setPeople(...people, newPerson.first_name);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="App">
-      <input value={search} onChange={handleSearch} />
+      <form>
+        <label htmlFor="first_name">Add person</label>
+        <input
+          type="text"
+          id="first_name"
+          name="first_name"
+          onChange={handleAddPerson}
+          value={formData.first_name}
+        />
+        <input type="submit" value="Submit" onClick={fetchAddPerson} />
+      </form>
+      <label htmlFor="search">Search people</label>
+      <input
+        type="text"
+        id="search"
+        name="search"
+        value={search}
+        onChange={handleSearch}
+      />
       <input type="button" value="Search" onClick={handleSubmit} />
       <ol>
         {people.map((person) => (
